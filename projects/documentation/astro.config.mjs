54466058b1d9430c96afd366e07711c9741134a6
jakeleveroni <jacobjleveroni@gmail.com>
2025-03-30 17:54:55 -0700

    add docs site

--- projects/documentation/astro.config.mjs
+++ projects/documentation/astro.config.mjs
@@ -1,5 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Levdev Labs",
+      social: {
+        github: "https://github.com/jakeleveroni/js-monorepo",
+      },
+      sidebar: [
+        {
+          label: "Repo",
+          autogenerate: { directory: "repo" },
+        },
+        {
+          label: "Projects",
+          autogenerate: { directory: "projects" },
+        },
+      ],
+    }),
+  ],
+});
