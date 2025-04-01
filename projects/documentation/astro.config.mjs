75680f425cb559717a737f72bc802c949344987e
jakeleveroni <jacobjleveroni@gmail.com>
2025-04-01 09:20:37 -0700

    add workflow to deploy astro site

--- projects/documentation/astro.config.mjs
+++ projects/documentation/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jakeleveroni.github.io",
+  base: "js-monorepo",
   integrations: [
     starlight({
       title: "Levdev Labs",
