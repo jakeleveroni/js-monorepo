2dac69249fe6dddbf5ce6718aee499836a2a986a
jakeleveroni <jacobjleveroni@gmail.com>
2025-04-02 21:50:14 -0700

    more logging

--- infra/get-package-json.ts
+++ infra/get-package-json.ts
@@ -46,11 +46,12 @@ function getPackageJson() {
 
     for (const ws of originalWorkspaces) {
       const glob = new Glob(ws);
-
+      console.log("original ws", getRootDir(), ws);
       for await (const pathMatch of glob.scan({
         cwd: getRootDir(),
         onlyFiles: false,
       })) {
+        console.log("match", pathMatch);
         const wsPkgJson = await packageJson(`${getRootDir()}/${pathMatch}`);
 
         explodedWorkspaces.push({
