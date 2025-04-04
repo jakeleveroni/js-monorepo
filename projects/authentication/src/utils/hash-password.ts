1d0948ac33c46dff30ce542654460f95efaafd29
jakeleveroni <jacobjleveroni@gmail.com>
2025-04-03 21:38:57 -0700

    test

--- projects/authentication/src/utils/hash-password.ts
+++ projects/authentication/src/utils/hash-password.ts
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 
-export async function saltAndHashPassword(username: string, password: string) {
+export async function saltAndHashPassword(password: string) {
   const salt = await bcrypt.genSalt(4);
   const hashed = await bcrypt.hash(password, salt);
   return {
