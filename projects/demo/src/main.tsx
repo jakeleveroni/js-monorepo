216fc8e4ea55b618571bae1a83dd951954656b63
jakeleveroni <jacobjleveroni@gmail.com>
2025-04-11 07:41:01 -0700

    formatting

--- projects/demo/src/main.tsx
+++ projects/demo/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './components/router.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
