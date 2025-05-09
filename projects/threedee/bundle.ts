345302013911b2a448488df76e8f682d31a987b3
jakeleveroni <jacobjleveroni@gmail.com>
2025-05-09 12:31:00 -0700

    get threedee build working

--- projects/threedee/bundle.ts
+++ projects/threedee/bundle.ts
@@ -1,7 +1,18 @@
-await Bun.build({
-  entrypoints: ['./src/main.tsx'],
-  target: 'bun',
-  outdir: './dist',
-});
-//asd
-export {};
+import { file } from 'bun';
+
+async function build() {
+  await Bun.build({
+    entrypoints: ['./src/main.tsx'],
+    target: 'browser', // Or your desired browser target
+    outdir: './dist',
+    packages: 'bundle',
+  });
+
+  const indexHtml = await file('./index.html').text();
+  const transpiledIndexHtml = indexHtml
+    .replaceAll('/src/main.tsx', './main.js')
+    .replaceAll('/src/styles.css', './main.css');
+  await file('./dist/index.html').write(transpiledIndexHtml);
+}
+
+build();
